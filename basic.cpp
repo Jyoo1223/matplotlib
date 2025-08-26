237771b6abeccdf11190a5b421f225bdd875738b
Jyoo1223 <jyoomadakala1223@gmail.com>
2025-08-26 10:12:25 +0530

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main(){
-    vector<int> a{1,3,5,8,9};
-    for(const auto& res:a){
-        cout<<resgit init<<"\n";
+    vector<int> a{1, 3, 5, 8, 9};  // Initialize the vector
+    for(const auto& res : a){       // Loop through each element in the vector
+        cout << res << "\n";         // Print the value of each element
     }
     return 0;
 }
